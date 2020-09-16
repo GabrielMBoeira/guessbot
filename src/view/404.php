31fa332bd77fe869766e609fe37923fdc6a16ced
GabrielMBoeira <gboeira777@gmail.com>
2020-09-15 22:57:11 -0300

    ajustando 404

--- src/view/404.php
+++ src/view/404.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/assets/css/404.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Guessbot</title>
 </head>
-<body>
+
+<body class="not_found">
     <h1>Pagina não encontrada</h1>
 </body>
 </html>
