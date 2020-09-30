4c0f33083e5a13788563a31c0948763eed724ff7
GabrielMBoeira <gboeira777@gmail.com>
2020-09-30 01:41:32 -0300

    convertendo ajax array to json

--- src/view/ajax_question.php
+++ src/view/ajax_question.php
@@ -7,7 +7,8 @@ $sql = "SELECT * from question";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    while ($dados = mysqli_fetch_array($result)) {
-        print_r($dados);
+    while ($dados = mysqli_fetch_assoc($result)) {
+        echo json_encode($dados);
     }
+    
 }
--- src/view/prank_user.php
+++ src/view/prank_user.php
@@ -1,31 +1,29 @@
 <?php
 require_once('template/header.php');
+
 ?>
 
 <link rel="stylesheet" href="src/assets/css/prank_user.css" />
 
 <main class="main">
     <div class="div-content">
+
+        <div class="id" id="id"></div>
+
         <div class="div-content-header bg-danger">
             <div class="title-box">Usuário chave</div>
         </div>
         <div class="div-content-body">
-
-
-            <div class="test" id="teste"></div>
-
-
-
             <form action="src/db/actions.php" method="post">
                 <div class="form-group">
                     <label for="question" class="label-question">O que você gostaria de saber?</label>
-                    <input type="text" class="form-control input-question" id="question" name="question" />
+                    <div class="div-question form-control" id="question"></div>
                 </div>
                 <div class="form-group">
-                    <label for="question" class="label-answer">Resposta:</label>
-                    <input type="text" class="form-control" id="" />
+                    <label for="question form-control" class="label-answer">Resposta:</label>
+                    <input type="text" class="form-control" />
                     <div class="button d-flex justify-content-end mt-2">
-                        <button class="btn btn-danger mt-1" id="submit" name="env-answer">Perguntar</button>
+                        <button class="btn btn-danger mt-1" id=" submit" name="env-answer">Responder</button>
                     </div>
                 </div>
             </form>
@@ -35,20 +33,26 @@ require_once('template/header.php');
 
 <script>
     function ajax() {
-            var req = new XMLHttpRequest();
-            req.onreadystatechange = function() {
-                if (req.readyState == 4 && req.status == 200) {
-                    document.getElementById('teste').innerHTML = req.responseText;
-                }
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+            if (req.readyState == 4 && req.status == 200) {
+
+                var resposta = JSON.parse(req.responseText)
+
+                console.log(resposta)
+
+                document.getElementById('question').innerHTML = resposta.question
+
             }
-            req.open('GET', 'testeajax', true);
-            req.send();
         }
+        req.open('POST', 'ajax_question', true);
+        req.send();
+    }
 
 
-        setInterval(function() {
-            ajax();
-        }, 1000);
+    setInterval(function() {
+        ajax();
+    }, 1000);
 </script>
 
 <?php
