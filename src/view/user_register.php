1153440fa596c33c153f255480467cac7d6c2e8b
GabrielMBoeira <gboeira777@gmail.com>
2020-11-04 00:22:56 -0300

    ajustando header user_register page

--- src/view/user_register.php
+++ src/view/user_register.php
@@ -1,5 +1,4 @@
 <?php
-require_once('template/header.php');
 require_once('src/db/connection.php');
 require_once('src/config/clear_input.php');
 require_once('src/config/functions.php');
@@ -36,53 +35,86 @@ if (isset($_POST['user_register'])) {
 
 ?>
 
-<link rel="stylesheet" href="src/assets/css/user_register.css" />
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<main class="main">
-    <div class="div-content">
-        <div class="container-fluid">
-            <div class="row">
-                <form class="form" action="#" method="post">
-                    <div class="header-form">
-                        <label>
-                            Faça teste gratuitamente
-                        </label>
-                    </div>
-                        <?php 
-                            if (isset($_SESSION['msg'])) {
-                                print_r($_SESSION['msg']);
-                                unset($_SESSION['msg']);
-                            } 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="src/assets/css/template.css" />
+    <title>Guessbot</title>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+
+<body class="body">
+    <header class="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="ask">Guessbot</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#content-nav" aria-controls="content-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="content-nav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Configurações</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <link rel="stylesheet" href="src/assets/css/user_register.css" />
+
+    <main class="main">
+        <div class="div-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <form class="form" action="#" method="post">
+                        <div class="header-form">
+                            <label>
+                                Faça teste gratuitamente
+                            </label>
+                        </div>
+                        <?php
+                        if (isset($_SESSION['msg'])) {
+                            print_r($_SESSION['msg']);
+                            unset($_SESSION['msg']);
+                        }
                         ?>
-                    <div class="form-group">
-                        <label class="label" for="email">
-                            Cadastre seu email
-                        </label>
-                        <input type="email" class="form-control" id="email" name="email" required value="<?= $dados['email']?>"/>
-                    </div>
-                    <div class="form-group">
-                        <label class="label" for="password">
-                            Cadastre sua senha
-                        </label>
-                        <input type="password" class="form-control" id="password" name="password" required />
-                    </div>
-                    <div class="form-group">
-                        <label class="label" for="password-confirm">
-                            Confirme sua senha
-                        </label>
-                        <input type="password" class="form-control" id="password-confirm" name="password-confirm" required />
-                    </div>
-                    <div class="div-button">
-                        <button type="submit" class="btn btn-primary mt-1" name="user_register">
-                            Cadastrar
-                        </button>
-                    </div>
-                </form>
+                        <div class="form-group">
+                            <label class="label" for="email">
+                                Cadastre seu email
+                            </label>
+                            <input type="email" class="form-control" id="email" name="email" required value="<?= $dados['email'] ?>" />
+                        </div>
+                        <div class="form-group">
+                            <label class="label" for="password">
+                                Cadastre sua senha
+                            </label>
+                            <input type="password" class="form-control" id="password" name="password" required />
+                        </div>
+                        <div class="form-group">
+                            <label class="label" for="password-confirm">
+                                Confirme sua senha
+                            </label>
+                            <input type="password" class="form-control" id="password-confirm" name="password-confirm" required />
+                        </div>
+                        <div class="div-button">
+                            <button type="submit" class="btn btn-primary mt-1" name="user_register">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
-<?php
-require_once('template/footer.php');
-?>
+    <?php
+    require_once('template/footer.php');
+    ?>
